5f4baef5230c3d004f98ff1c1e72c5f31b0a06d2
Michael Lawlor <mikelawlorarb@outlook.com>
2024-11-17 17:34:44 +0000

    add project summary

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 
 # load pages scripts
-#from app_pages.page_summary import page_summary_body
 from app_pages.page_findings import page_findings_body
 from app_pages.page_hypothesis import page_hypothesis_body
 from app_pages.page_mildew_detector import page_mildew_detector_body
 from app_pages.page_ml_performance import page_ml_performance_body
-#from app_pages.page_project_summary import page_project_summary_body
+from app_pages.page_project_summary import page_project_summary_body
 # Create an instance of the app
 app = MultiPage(app_name="Mildew Detector") 
 # app pages
-#app.add_page("Project Summary", page_project_summary_body)
+app.add_page("Project Summary", page_project_summary_body)
 app.add_page("Findings", page_findings_body)
 app.add_page("Hypothesis", page_hypothesis_body)
 app.add_page("Mildew Detector", page_mildew_detector_body)
