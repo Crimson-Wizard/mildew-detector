d6d49d4b33725679452c0f029c5ca5da8ce3be82
Michael Lawlor <mikelawlorarb@outlook.com>
2024-11-20 16:28:45 +0000

    add hardcode image size

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -7,7 +7,6 @@ from PIL import Image
 from src.data_management import load_pkl_file
 
 
-
 def plot_predictions_probabilities(pred_proba, pred_class):
     """
     Plot prediction probability results
@@ -39,9 +38,9 @@ def resize_input_image(img):
     Reshape image to average image size
     """
     try:
-        image_shape = load_pkl_file(file_path=f"outputs/v2/image_shape.pk1")
+        image_shape = (256, 256)
         #resize image
-        img_resized = img.resize((image_shape[1], image_shape[0]), Image.LANCZOS)
+        img_resized = img.resize(image_shape, Image.LANCZOS)
         my_image = np.expand_dims(img_resized, axis=0)/255
 
         return my_image
