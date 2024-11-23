be876b208ddc9e2179de3ac1666ddc0000b0bceb
Michael Lawlor <mikelawlorarb@outlook.com>
2024-11-23 11:17:30 +0000

    add .h5 to replace pb file

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -58,7 +58,7 @@ def load_model_and_predict(my_image):
         #tensoreflow version
         st.write(f"TensorFlow version: {tf.__version__}")
         
-        model = load_model('path_to_save/saved_model')
+        model = load_model('outputs/trained_model.h5')
 
         pred_proba = model.predict(my_image)[0, 0]
 
