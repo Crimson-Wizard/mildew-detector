10b0a30c8405f7ca3fc1bb8983d070702042e516
Michael Lawlor <mikelawlorarb@outlook.com>
2024-11-23 12:15:57 +0000

    retry svaed_model.pb

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -58,7 +58,7 @@ def load_model_and_predict(my_image, version):
         #tensoreflow version
         st.write(f"TensorFlow version: {tf.__version__}")
         
-        model = load_model(f"outputs/{version}/trained_model.h5")
+        model = load_model("path_to_save/saved_model")
 
         pred_proba = model.predict(my_image)[0, 0]
 
