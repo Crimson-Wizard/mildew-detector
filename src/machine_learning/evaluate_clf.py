a5704c2738f741d3231a776798c84cc4cf7b4640
Michael Lawlor <mikelawlorarb@outlook.com>
2024-11-24 15:49:45 +0000

    add correct version for performance model test set

--- src/machine_learning/evaluate_clf.py
+++ src/machine_learning/evaluate_clf.py
@@ -4,4 +4,4 @@ from src.data_management import load_pkl_file
 
 # Function to load a test evaluation object from a specific pickle file
 def load_test_evaluation():
-    return load_pkl_file('outputs/v3/evaluation.pk1')
+    return load_pkl_file('outputs/v4/evaluation.pk1')
